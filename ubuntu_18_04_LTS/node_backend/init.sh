56b0c1b67c2265cea8d9d8c31d0515f6ece1775f
HADMARINE <contact@hadmarine.com>
2020-05-19 15:24:06 +0900

    Update scripts

--- ubuntu_18_04_LTS/node_backend/init.sh
+++ ubuntu_18_04_LTS/node_backend/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function execute() {
-  sudo apt update
+  sudo apt update -y
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
