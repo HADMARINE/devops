291e0ca1513663275567062f60067672bb031f59
HADMARINE <contact@hadmarine.com>
2020-05-19 15:55:52 +0900

    Update scripts

--- ubuntu_18_04_LTS/aws/codedeploy/init.sh
+++ ubuntu_18_04_LTS/aws/codedeploy/init.sh
@@ -4,6 +4,7 @@
 
 sudo apt update -y
 sudo apt install -y ruby
-wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+bash <(curl https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install)
 chmod +x ./install
 sudo ./install auto
+sudo rm -rf ./install
