a779c2a620324500b83682f4e198edb7304b4e84
HADMARINE <contact@hadmarine.com>
2020-05-20 08:27:40 +0900

    Update scripts
    debug aws

--- ubuntu_18_04_LTS/aws/codedeploy/init.sh
+++ ubuntu_18_04_LTS/aws/codedeploy/init.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # This Script installs AWS code-deploy agent
 
-sudo apt update -y
-sudo apt install -y ruby
+cd /home/ubuntu
+apt update -y
+apt install -y ruby wget
 wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto
-sudo rm -rf ./install
-sudo service codedeploy-agent start
+./install auto
+rm -rf ./install
+service codedeploy-agent restart
