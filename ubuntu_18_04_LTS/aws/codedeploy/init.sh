67fdcc59a623cd851f34497bf62d60f35a539446
HADMARINE <contact@hadmarine.com>
2020-05-19 15:16:29 +0900

    Create scripts (AWS)

---
+++ ubuntu_18_04_LTS/aws/codedeploy/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This Script installs AWS code-deploy agent
+
+sudo apt update -y
+sudo apt install -y ruby
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
