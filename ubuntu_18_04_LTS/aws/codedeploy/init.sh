b8b069ab5994379403dda8d2fb961d43522f11b3
HADMARINE <tsx@kakao.com>
2021-06-10 20:10:54 +0900

    Update init.sh

--- ubuntu_18_04_LTS/aws/codedeploy/init.sh
+++ ubuntu_18_04_LTS/aws/codedeploy/init.sh
@@ -8,9 +8,10 @@ sudo apt install -y ruby
 
 until service codedeploy-agent status >/dev/null 2>&1; do
    sleep 30
-    sudo rm -rf install
-    wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
-    chmod +x ./install
-    sudo ./install auto
-    sudo service codedeploy-agent restart
+   sudo rm -rf install
+   wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+   chmod +x ./install
+   sudo ./install auto
+   sudo service codedeploy-agent restart
+   sudo rm -rf install
 done
